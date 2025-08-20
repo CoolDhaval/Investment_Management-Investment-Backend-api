e8ded19baf22e3b882b7c5a40146c565d0c99f41
CoolDhaval <Dhaval456Shah2013@gmail.com>
2025-08-21 01:19:50 +0530

    updated app.js for cors setting to e access from my github IO

--- app.js
+++ app.js
@@ -6,11 +6,30 @@ dotenv.config();
 const bodyParser = require('body-parser');
 
 //app.use(cors()); // <--- allow all origins (or restrict below)
+/*
 app.use(cors({
     origin: 'http://localhost:4300',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
   }));
+  */
+  const allowedOrigins = [
+    'http://localhost:4300',
+    'https://cooldhaval.github.io/'
+  ];
+  
+  app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
+  
 const companyRoutes = require('./routes/companyRoutes');
 const clientRoutes = require('./routes/clientRoutes');
 const schemeRoutes = require('./routes/schemeRoutes');
